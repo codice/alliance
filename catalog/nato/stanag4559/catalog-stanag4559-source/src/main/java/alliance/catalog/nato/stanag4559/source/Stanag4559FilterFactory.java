f8bcc19107542545b58f4345a69c9ce191d2850f
bdeining <benjamin.deininger@connexta.com>
2016-02-29 10:16:57 -0700

    DDF-1864 Added Sample Stanag 4559 Server DDF-1880 Added Stanag 4559 Source and Filter Delegate DDF-1903 Added temporal queries DDF-1891: Additional unit tests for the various types of NSIL_XXX views

---
+++ catalog/nato/stanag4559/catalog-stanag4559-source/src/main/java/alliance/catalog/nato/stanag4559/source/Stanag4559FilterFactory.java
@@ -0,0 +1,203 @@
+/**
+ * Copyright (c) Codice Foundation
+ * <p>
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package alliance.catalog.nato.stanag4559.source;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import alliance.catalog.nato.stanag4559.common.GIAS.AttributeInformation;
+import alliance.catalog.nato.stanag4559.common.GIAS.AttributeType;
+import alliance.catalog.nato.stanag4559.common.Stanag4559Constants;
+
+import ddf.catalog.data.Metacard;
+
+public class Stanag4559FilterFactory {
+
+    public static final String TYPE = "type";
+
+    public static final String LP = "(";
+
+    public static final String RP = ")";
+
+    public static final String LIKE = " like ";
+
+    public static final String OR = " or ";
+
+    public static final String AND = " and ";
+
+    public static final String EXISTS = " exists";
+
+    public static final String NOT = "not ";
+
+    public static final String EQ = " = ";
+
+    public static final String LT = " < ";
+
+    public static final String GT = " > ";
+
+    public static final String LTE = " <= ";
+
+    public static final String GTE = " >= ";
+
+    public static final String BTW = " <> ";
+
+    public static final String COMMA = ",";
+
+    private HashMap<String, List<AttributeInformation>> queryableAttributes;
+
+    private String view;
+
+    public Stanag4559FilterFactory(HashMap<String, List<AttributeInformation>> queryableAttributes,
+            String view) {
+        this.queryableAttributes = queryableAttributes;
+        this.view = view;
+    }
+
+    public String buildPropertyIsLike(String value) {
+        if (queryableAttributes == null) {
+            return Stanag4559FilterDelegate.EMPTY_STRING;
+        }
+
+        List<AttributeInformation> attributeInformationList = queryableAttributes.get(view);
+
+        if (attributeInformationList == null) {
+            return Stanag4559FilterDelegate.EMPTY_STRING;
+        }
+
+        List<String> filters = new ArrayList<>();
+
+        // Replace * with %, since * is not a valid wildcard in BQS
+        value = value.replaceAll("\\*", "%");
+        value = value.replaceAll("\\?", "%");
+
+        for (AttributeInformation attributeInformation : attributeInformationList) {
+            if (isTextAttributeType(attributeInformation)) {
+                filters.add(LP + attributeInformation.attribute_name + LIKE
+                        + Stanag4559FilterDelegate.SQ + value + Stanag4559FilterDelegate.SQ + RP);
+            }
+        }
+        return buildOrFilter(filters);
+    }
+
+    public boolean isTextAttributeType(AttributeInformation attributeInformation) {
+        return attributeInformation.attribute_type.equals(AttributeType.TEXT)
+                && !attributeInformation.attribute_name.equals(TYPE);
+    }
+
+    public String buildPropertyIsEqualTo(String property, String value) {
+        property = mapToNsil(property);
+        return LP + property + EQ + value + RP;
+    }
+
+    public String buildPropertyIsNotEqualTo(String property, String value) {
+        property = mapToNsil(property);
+        return buildNotFilter(buildPropertyIsEqualTo(property, value));
+    }
+
+    public String buildPropertyIsGreaterThan(String property, String value) {
+        property = mapToNsil(property);
+        return LP + property + GT + value + RP;
+    }
+
+    public String buildPropertyIsGreaterThanOrEqual(String property, String value) {
+        property = mapToNsil(property);
+        return LP + property + GTE + value + RP;
+    }
+
+    public String buildPropertyIsLessThan(String property, String value) {
+        property = mapToNsil(property);
+        return LP + property + LT + value + RP;
+    }
+
+    public String buildPropertyIsLessThanOrEqual(String property, String value) {
+        property = mapToNsil(property);
+        return LP + property + LTE + value + RP;
+    }
+
+    public String buildPropertyIsBetween(String property, String lowerBound, String upperBound) {
+        property = mapToNsil(property);
+        return LP + property + BTW + lowerBound + COMMA + upperBound + RP;
+    }
+
+    public String buildOrFilter(List<String> filters) {
+        if (filters.size() == 0) {
+            return Stanag4559FilterDelegate.EMPTY_STRING;
+        }
+
+        if (filters.size() == 1) {
+            return filters.get(0);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder(LP);
+
+        for (String filter : filters) {
+            if (StringUtils.isNotBlank(filter)) {
+                stringBuilder.append(filter + OR);
+            }
+        }
+
+        String result = stringBuilder.toString();
+        if (result.length() > 1) {
+            return result.substring(0, result.length() - 4) + RP;
+        }
+        return Stanag4559FilterDelegate.EMPTY_STRING;
+    }
+
+    public String buildAndFilter(List<String> filters) {
+        if (filters.size() == 0) {
+            return Stanag4559FilterDelegate.EMPTY_STRING;
+        }
+
+        if (filters.size() == 1) {
+            return filters.get(0);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder(LP);
+
+        for (String filter : filters) {
+            if (StringUtils.isNotBlank(filter)) {
+                stringBuilder.append(filter + AND);
+            }
+        }
+        String result = stringBuilder.toString();
+        if (result.length() > 1) {
+            return result.substring(0, result.length() - 5) + RP;
+        }
+        return Stanag4559FilterDelegate.EMPTY_STRING;
+    }
+
+    public String buildPropertyIsNull(String property) {
+        return buildNotFilter(LP + property + EXISTS + RP);
+    }
+
+    public String buildNotFilter(String filter) {
+        return NOT + filter;
+    }
+
+    public static String mapToNsil(String attribute) {
+        if (attribute.equals(Metacard.CONTENT_TYPE)) {
+            return TYPE;
+        } else if (attribute.equals(Metacard.EFFECTIVE)) {
+            return Stanag4559Constants.DATE_TIME_DECLARED;
+        } else if (attribute.equals(Metacard.CREATED)) {
+            return Stanag4559Constants.DATE_TIME_DECLARED;
+        } else if (attribute.equals(Metacard.MODIFIED)) {
+            return Stanag4559Constants.DATE_TIME_MODIFIED;
+        }
+        return attribute;
+    }
+}
