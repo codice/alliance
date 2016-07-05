0ceb5a96644d9e03eaaa651d8889d0d0d5c7ddb7
Glen Hein <glen.hein@connexta.com>
2016-07-05 10:39:37 -0700

    CAL-90 compiles and tests

--- libs/klv/src/main/java/org/codice/alliance/libs/klv/LocationCountryCodeKlvProcessor.java
+++ libs/klv/src/main/java/org/codice/alliance/libs/klv/LocationCountryCodeKlvProcessor.java
@@ -16,7 +16,7 @@ package org.codice.alliance.libs.klv;
 import org.codice.alliance.libs.stanag4609.Stanag4609TransportStreamParser;
 
 public class LocationCountryCodeKlvProcessor extends DistinctKlvProcessor {
-    protected LocationCountryCodeKlvProcessor() {
+    public LocationCountryCodeKlvProcessor() {
         super(AttributeNameConstants.LOCATION_COUNTY_CODE,
                 Stanag4609TransportStreamParser.CLASSIFYING_COUNTRY);
     }
