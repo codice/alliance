e1a78d73087a46213fbd8935bb656bcd405350db
Brandan Jeter <brandanjeter@gmail.com>
2017-12-21 14:45:39 -0700

    CAL-390 Add validation for KLV processing for dissemination and SCI Controls

--- libs/klv/src/test/java/org/codice/alliance/libs/klv/UtilitiesTest.java
+++ libs/klv/src/test/java/org/codice/alliance/libs/klv/UtilitiesTest.java
@@ -92,6 +92,12 @@ public class UtilitiesTest {
   }
 
   @Test
+  public void testIsEmptryStringUsingBlankAttribute() throws Exception {
+    assertThat(Utilities.isBlankString(" "), is(true));
+    assertThat(Utilities.isNotBlankString(" "), is(false));
+  }
+
+  @Test
   public void testNullDescriptorDoesNotSetAttribute() throws Exception {
     AttributeImpl attribute = new AttributeImpl(ATTRIBUTE_KEY_POC, EXPECTED_POC);
     Metacard mockedMetacard = setupMetacardWithNullDescriptor();
