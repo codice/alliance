2d07408823707799d086f276c3e33c04f2d58fb1
clockard <chris.lockard@connexta.com>
2022-04-28 20:01:10 -0700

    Add klv processing to udp stream processor

--- catalog/video/video-mpegts-stream/src/main/java/org/codice/alliance/video/stream/mpegts/plugins/ParentMetacardStreamCreationPlugin.java
+++ catalog/video/video-mpegts-stream/src/main/java/org/codice/alliance/video/stream/mpegts/plugins/ParentMetacardStreamCreationPlugin.java
@@ -14,17 +14,32 @@
 package org.codice.alliance.video.stream.mpegts.plugins;
 
 import static org.apache.commons.lang3.Validate.notNull;
+import static org.codice.alliance.catalog.core.api.types.Isr.ORIGINAL_SOURCE;
 
 import ddf.catalog.CatalogFramework;
+import ddf.catalog.data.Attribute;
 import ddf.catalog.data.Metacard;
 import ddf.catalog.data.MetacardCreationException;
 import ddf.catalog.data.MetacardType;
+import ddf.catalog.data.Result;
+import ddf.catalog.data.impl.AttributeImpl;
 import ddf.catalog.data.impl.MetacardImpl;
+import ddf.catalog.data.types.Associations;
+import ddf.catalog.data.types.Core;
+import ddf.catalog.federation.FederationException;
+import ddf.catalog.filter.FilterBuilder;
 import ddf.catalog.operation.CreateRequest;
 import ddf.catalog.operation.impl.CreateRequestImpl;
+import ddf.catalog.operation.impl.QueryImpl;
+import ddf.catalog.operation.impl.QueryRequestImpl;
+import ddf.catalog.operation.impl.UpdateRequestImpl;
 import ddf.catalog.source.IngestException;
 import ddf.catalog.source.SourceUnavailableException;
+import ddf.catalog.source.UnsupportedQueryException;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.codice.alliance.video.stream.mpegts.Constants;
 import org.codice.alliance.video.stream.mpegts.Context;
@@ -39,6 +54,8 @@ public class ParentMetacardStreamCreationPlugin extends BaseStreamCreationPlugin
   private final CatalogFramework catalogFramework;
 
   private final List<MetacardType> metacardTypeList;
+
+  private FilterBuilder filterBuilder;
 
   /**
    * @param catalogFramework must be non-null
@@ -65,6 +82,8 @@ public class ParentMetacardStreamCreationPlugin extends BaseStreamCreationPlugin
     setParentResourceUri(context, metacard);
     setParentTitle(context, metacard);
     setParentContentType(metacard);
+    setParentVideoSource(context, metacard);
+    setParentOriginalUrl(context, metacard);
 
     CreateRequest createRequest = new CreateRequestImpl(metacard);
 
@@ -73,6 +92,8 @@ public class ParentMetacardStreamCreationPlugin extends BaseStreamCreationPlugin
     } catch (IngestException | SourceUnavailableException e) {
       throw new StreamCreationException("unable to submit parent metacard to catalog framework", e);
     }
+
+    linkSourceWithParent(context);
   }
 
   private MetacardCreationException createException() {
@@ -88,12 +109,33 @@ public class ParentMetacardStreamCreationPlugin extends BaseStreamCreationPlugin
     context.getUdpStreamProcessor().getStreamUri().ifPresent(metacard::setResourceURI);
   }
 
+  private void setParentVideoSource(Context context, MetacardImpl metacard) {
+    addRelation(
+        metacard,
+        context.getUdpStreamProcessor().getAdditionalProperties().get("video-source-id"),
+        Associations.DERIVED);
+  }
+
+  private void setParentOriginalUrl(Context context, MetacardImpl metacard) {
+    LOGGER.debug(
+        "Adding origin url of {} to {}",
+        context.getUdpStreamProcessor().getAdditionalProperties().get("original-url"),
+        metacard.getId());
+    metacard.setAttribute(
+        ORIGINAL_SOURCE,
+        context.getUdpStreamProcessor().getAdditionalProperties().get("original-url"));
+  }
+
   private void setParentContentType(MetacardImpl metacard) {
     metacard.setContentTypeName(Constants.MPEGTS_MIME_TYPE);
   }
 
   private void setParentTitle(Context context, MetacardImpl metacard) {
-    context.getUdpStreamProcessor().getTitle().ifPresent(metacard::setTitle);
+    // a new metacard is created each time recording is started
+    context
+        .getUdpStreamProcessor()
+        .getTitle()
+        .ifPresent(title -> metacard.setTitle(title + " - Rec " + context.getNextRecordingCount()));
   }
 
   private void submitParentCreateRequest(Context context, CreateRequest createRequest)
@@ -103,5 +145,61 @@ public class ParentMetacardStreamCreationPlugin extends BaseStreamCreationPlugin
         createdMetacards.stream().map(Metacard::getId).collect(Collectors.toList());
     LOGGER.debug("created parent metacards with ids: {}", createdIds);
     context.setParentMetacard(createdMetacards.get(createdMetacards.size() - 1));
+  }
+
+  private void linkSourceWithParent(Context context) {
+    String videoSourceId =
+        context.getUdpStreamProcessor().getAdditionalProperties().get("video-source-id");
+    LOGGER.debug("Linking source with parent id: {}", videoSourceId);
+    if (videoSourceId != null) {
+      try {
+        Metacard source =
+            catalogFramework
+                .query(
+                    new QueryRequestImpl(
+                        new QueryImpl(
+                            filterBuilder.attribute(Core.ID).is().equalTo().text(videoSourceId))))
+                .getResults().stream()
+                .map(Result::getMetacard)
+                .findFirst()
+                .orElse(null);
+        if (source == null) {
+          LOGGER.warn("Couldn't find video source metacard with id {}", videoSourceId);
+          return;
+        }
+        context
+            .getParentMetacard()
+            .ifPresent(
+                parent -> {
+                  addRelation(source, parent.getId(), Associations.RELATED);
+                  try {
+                    catalogFramework.update(new UpdateRequestImpl(source.getId(), source));
+                  } catch (IngestException | SourceUnavailableException e) {
+                    LOGGER.warn(
+                        "Failed to update video source association with id {}", videoSourceId, e);
+                  }
+                });
+      } catch (UnsupportedQueryException | SourceUnavailableException | FederationException e) {
+        LOGGER.warn("Failed to locate video source metacard with id {}", videoSourceId, e);
+      }
+    }
+  }
+
+  private void addRelation(Metacard metacard, String id, String type) {
+    if (metacard != null && id != null) {
+      LOGGER.debug("Adding relation from {} to {}", metacard.getTitle(), id);
+      List<Serializable> related =
+          Optional.ofNullable(metacard.getAttribute(type))
+              .map(Attribute::getValues)
+              .orElse(new ArrayList<>());
+      if (!related.contains(id)) {
+        related.add(id);
+        metacard.setAttribute(new AttributeImpl(type, related));
+      }
+    }
+  }
+
+  public void setFilterBuilder(FilterBuilder filterBuilder) {
+    this.filterBuilder = filterBuilder;
   }
 }
